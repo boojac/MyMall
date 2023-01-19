85cd5060a35d131ec0eac3ed7785fadd2fdcd21e
boojac <13590912671@163.com>
2023-01-19 21:53:17 +0800

    jsonp

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,30 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
+// import axios from 'axios'
+import jsonp from 'jsonp'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
   },
-  mounted() {
-    let url = "https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list";
-    axios.get(url).then(()=>{
-      
+  // mounted() {
+  //   let url = "https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list";
+  //   axios.get(url).then(()=>{
+    //axios的跨域方式
+  //   })
+  // },
+  data() {
+    return {
+      data:''
+    }
+  },
+  mouted(){
+    let url = "http://www.imooc/activity/servicetime";
+    jsonp(url,(err, res)=>{
+      let result = res;
+      this.data = result;
     })
   },
 }
