d861de8cdf5bb6f369816032201596a3607405d9
boojac <13590912671@163.com>
2023-01-20 11:30:04 +0800

    项目目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 // import axios from 'axios'
-import jsonp from 'jsonp'
+// import jsonp from 'jsonp'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
-  // mounted() {
-  //   let url = "https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list";
-  //   axios.get(url).then(()=>{
-    //axios的跨域方式
-  //   })
-  // },
   data() {
     return {
       data:''
     }
   },
-  mouted(){
-    let url = "http://www.imooc/activity/servicetime";
-    jsonp(url,(err, res)=>{
-      let result = res;
-      this.data = result;
-    })
+  mounted(){
+
   },
 }
 </script>
