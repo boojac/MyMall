47ad3ce895016e51e0ecae8e627d948f1c773668
boojac <13590912671@163.com>
2023-01-21 14:58:48 +0800

    接口环境设置

---
+++ src/env.js
@@ -0,0 +1,23 @@
+let baseURL;
+//根据不同环境输出url地址,当使用非代理时，这样做，
+//基于jsonp、crocs时使用
+switch (process.env.NODE_ENV) {
+    case 'dev':
+        baseURL = 'http://dev-mall-pre.springboot.cn/api';
+        break;
+    case 'test':
+        baseURL = 'http://test-mall-pre.springboot.cn/api';
+        break;
+    case 'prev':
+        baseURL = 'http://prev-mall-pre.springboot.cn/api';
+        break;
+    case 'prod':
+        baseURL = 'http://mall-pre.springboot.cn/api';
+        break;
+    default:
+        baseURL = 'http://mall-pre.springboot.cn/api';
+        break;
+}
+export default {
+    baseURL
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import env from './env'
 
 // 插件放上面，组件放下面
 import App from './App.vue'
@@ -9,6 +10,8 @@ import App from './App.vue'
 //设置基础值,根据前端的跨域方式做调整,此为接口代理方式
 axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000;
+//根据环境变量获取不同的请求地址
+axios.defaults.baseURL = env.baseURL;
 //错误拦截代码
 axios.interceptors.response.use(function(response){
   let res = response.data;
